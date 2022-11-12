ccb1a87da141a9815a949208f164f8049ae51585
anhquan1301 <nlaquan1301@gmail.com>
2022-11-12 17:00:08 +0700

    fixed baitap

--- src/ss8_cau_truc_dieu_kien_1/luyen_tap_cau_truc_dieu_kien_1/bai_tap_5/bai_tap_5.js
+++ src/ss8_cau_truc_dieu_kien_1/luyen_tap_cau_truc_dieu_kien_1/bai_tap_5/bai_tap_5.js
@@ -1,8 +1,8 @@
 function poin(){
-    let poin1 = +document.getElementById("poin1").value;
-    let poin2 = +document.getElementById("poin2").value;
-    let poin3 = +document.getElementById("poin3").value;
-    let result = (poin1 + poin2*2 + poin3*3)/6;
+    let point1 = +document.getElementById("point1").value;
+    let point2 = +document.getElementById("point2").value;
+    let point3 = +document.getElementById("point3").value;
+    let result = (point1 + point2*2 + point3*3)/6;
         if (result >= 8)
             result = "Học sinh giỏi"
         else if (result >= 6.5)
