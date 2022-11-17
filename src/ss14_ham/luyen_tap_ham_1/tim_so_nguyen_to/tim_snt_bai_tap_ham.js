edaf4fdaa71d9229ed13a35e60bb9e3be9602196
anhquan1301 <nlaquan1301@gmail.com>
2022-11-17 17:16:06 +0700

    update bai tap

---
+++ src/ss14_ham/luyen_tap_ham_1/tim_so_nguyen_to/tim_snt_bai_tap_ham.js
@@ -0,0 +1,13 @@
+function isPrimeNumber(n) {
+    let count = 0;
+    for (let i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            count++;
+        }
+    }
+    if (count == 2) {
+        return true;
+    } else {
+        return false;
+    }
+}
