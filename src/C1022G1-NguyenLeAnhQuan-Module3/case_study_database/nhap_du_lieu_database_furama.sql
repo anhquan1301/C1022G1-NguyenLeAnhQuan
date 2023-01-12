e81cacecf8d0aa56b173cb2866e001b3d82049d9
anhquan1301 <nlaquan1301@gmail.com>
2023-01-12 21:06:44 +0700

    update

--- src/C1022G1-NguyenLeAnhQuan-Module3/case_study_database/nhap_du_lieu_database_furama.sql
+++ src/C1022G1-NguyenLeAnhQuan-Module3/case_study_database/nhap_du_lieu_database_furama.sql
@@ -45,11 +45,11 @@ insert into kieu_thue (ten_kieu_thue) values ('year'),('month'),('day'),('hour')
 -- Dịch vụ
 insert into dich_vu (ten_dich_vu, dien_tich, chi_phi_thue, so_nguoi_toi_da, tieu_chuan_phong,
  mo_ta_tien_nghi_khac, dien_tich_ho_boi, so_tang, dich_vu_mien_phi_di_kem, ma_kieu_thue, ma_loai_dich_vu) values 
-('Villa Beach Front', 25000, 1000000, 10, 'vip', 'Có hồ bơi', 500, 4, 'null', 3, 1),
-('House Princess 01', 14000, 5000000, 7, 'vip', 'Có thêm bếp nướng', null, 3, 'null', 2, 2),
+('Villa Beach Front', 25000, 1000000, 10, 'vip', 'Có hồ bơi', 500, 4, null, 3, 1),
+('House Princess 01', 14000, 5000000, 7, 'vip', 'Có thêm bếp nướng', null, 3, null, 2, 2),
 ('Room Twin 01', '5000', 1000000, 2, 'normal', 'Có tivi', null, null, '1 Xe máy, 1 Xe đạp', 4, 3),
-('Villa No Beach Front', 22000, 9000000, 8, 'normal', 'Có hồ bơi', 300, 3, 'null', 3, 1),
-('House Princess 02', 10000, 4000000, 5, 'normal', 'Có thêm bếp nướng', null, 2, 'null', 3, 2),
+('Villa No Beach Front', 22000, 9000000, 8, 'normal', 'Có hồ bơi', 300, 3, null, 3, 1),
+('House Princess 02', 10000, 4000000, 5, 'normal', 'Có thêm bếp nướng', null, 2, null, 3, 2),
 ('Room Twin 02', 3000, 900000, 2, 'normal', 'Có tivi', null, null, '1 Xe máy', 4, 3);
 
 -- Dịch vụ đi kèm
