db33b336ebe0790d332498e404ad9081dc271c6c
anhquan1301 <nlaquan1301@gmail.com>
2023-02-01 20:41:26 +0700

    update

--- src/C1022G1-NguyenLeAnhQuan-Module3/case_study_database/truy_van_du_lieu_database_furama.sql
+++ src/C1022G1-NguyenLeAnhQuan-Module3/case_study_database/truy_van_du_lieu_database_furama.sql
@@ -133,7 +133,7 @@ SELECT
 FROM
     hop_dong
 WHERE
-    YEAR(ngay_lam_hop_dong) = '2021'
+    YEAR(ngay_lam_hop_dong) = 2021
 GROUP BY thang
 ORDER BY thang;
 -- 10.	Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong, 
@@ -143,10 +143,62 @@ SELECT
     ngay_lam_hop_dong,
     ngay_ket_thuc,
     tien_dat_coc,
-    IFNULL(SUM(so_luong), 0) AS so_luong_dich_vu_di_kem
+    IFNULL(SUM(so_luong), 0) so_luong_dich_vu_di_kem
 FROM
     hop_dong hd
         LEFT JOIN
     hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
 GROUP BY hd.ma_hop_dong;
- 
+-- 11.Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng bởi những khách hàng có ten_loai_khach là “Diamond” và có dia_chi ở “Vinh” hoặc “Quảng Ngãi”.
+ select dvdk.ma_dich_vu_di_kem, dvdk.ten_dich_vu_di_kem from khach_hang kh
+ join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
+ join hop_dong hd on kh.ma_khach_hang = hd.ma_khach_hang 
+ join hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
+ join dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+ where lk.ten_loai_khach = 'Diamond' and kh.dia_chi like '% Vinh' or kh.dia_chi like '% Quảng Ngãi';
+ -- 12.Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng), 
+ -- so_dien_thoai (khách hàng), ten_dich_vu, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), 
+ -- tien_dat_coc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021. 
+select hd.ma_hop_dong,nv.ho_ten,kh.ho_ten,kh.so_dien_thoai,dv.ten_dich_vu,ifnull(sum(so_luong),0) so_luong_dich_vu_di_kem,tien_dat_coc 
+from hop_dong hd 
+join nhan_vien nv on hd.ma_nhan_vien = nv.ma_nhan_vien
+join khach_hang kh on hd.ma_khach_hang = kh.ma_khach_hang
+join dich_vu dv on hd.ma_dich_vu = dv.ma_dich_vu
+left join hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
+where (quarter(ngay_lam_hop_dong)=4 and year(ngay_lam_hop_dong)=2020)
+and hd.ma_hop_dong not in (month(ngay_lam_hop_dong) between 1 and 6 and year(ngay_lam_hop_dong) = 2021)
+group by hd.ma_hop_dong;
+-- 13.Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. 
+-- (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
+select dvdk.ma_dich_vu_di_kem, ten_dich_vu_di_kem, ifnull(sum(so_luong),0) so_luong_dich_vu_chi_tiet
+from dich_vu_di_kem dvdk
+join hop_dong_chi_tiet hdct on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+group by dvdk.ma_dich_vu_di_kem
+having so_luong_dich_vu_chi_tiet = (select max(so_luong) from hop_dong_chi_tiet);
+-- 14.Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. 
+-- Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung (được tính dựa trên việc count các ma_dich_vu_di_kem).
+set sql_mode=(select replace(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+select hd.ma_hop_dong,ten_loai_dich_vu,ten_dich_vu_di_kem, count(hdct.ma_hop_dong_chi_tiet) so_lan_su_dung
+from hop_dong hd
+join dich_vu dv on dv.ma_dich_vu = hd.ma_dich_vu
+join loai_dich_vu ldv on ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu
+join hop_dong_chi_tiet hdct on hdct.ma_hop_dong = hd.ma_hop_dong
+join dich_vu_di_kem dvdk on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+group by hdct.ma_dich_vu_di_kem
+having so_lan_su_dung = 1
+order by hd.ma_hop_dong;
+-- 15.Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan, so_dien_thoai, 
+-- dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
+select nv.ma_nhan_vien,ho_ten,ten_trinh_do,ten_bo_phan,so_dien_thoai,dia_chi,
+count(hd.ma_hop_dong) so_lan_su_dung
+from hop_dong hd
+join nhan_vien nv on nv.ma_nhan_vien = hd.ma_nhan_vien
+join trinh_do td on td.ma_trinh_do = nv.ma_trinh_do
+join bo_phan bp on bp.ma_bo_phan = nv.ma_bo_phan
+group by hd.ma_nhan_vien
+having so_lan_su_dung < 4;
+
+
+
+
+
